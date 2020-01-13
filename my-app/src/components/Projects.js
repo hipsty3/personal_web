94bd292869dc8864e2354bab1d5786bf1f32a406
Raisa <ramalia@student.unimelb.edu.au>
2020-01-13 12:14:04 +0700

    projects page

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -2,11 +2,17 @@ import React from 'react';
 import me from './../me.jpg';
 
 export default function Projects() {
-  return (
-    <div className="content">
-	  <div>
-	  	<h1>Hello Projects</h1>
-      </div>
-    </div>
-  )
+	return (
+		<div className="content">
+			<div>
+				<h1>Projects</h1>
+					<ul>
+						<li>GPS: Get People Socializing</li>
+						<li>Mementos</li>
+						<li>This website!</li>
+					</ul>
+
+			</div>
+		</div>
+	)
 }
