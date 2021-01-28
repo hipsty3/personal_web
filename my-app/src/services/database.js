44fa0db8d58a13fe68b0be7ea14710f36bae5d9f
Raisa Amalia <raisaamaliaa@gmail.com>
2021-01-29 10:51:29 +1100

    insert projects to database

--- my-app/src/services/database.js
+++ my-app/src/services/database.js
@@ -98,6 +98,92 @@ const projects = [
         desc: 'Android app that keeps track of family heirloom to be passed down to future generations.',
         tools: []
       },
+      {
+        id: 1,
+        title: 'Sign Up & Sign In',
+        image: '',
+        desc: `* Sign-up and login using email
+        * Implement email and input verification
+        * After signed up or logged in, the app will remember the user's credentials until they log out.`,
+        tools: []
+      },
+      {
+        id: 2,
+        title: 'Profile',
+        image: '',
+        desc: `* Display the user's name and date of birth.
+        * Display the user's profile picture from Firebase.
+        * Display artefacts created by the user in a 3 column grid format.`,
+        tools: []
+      },
+      {
+        id: 3,
+        title: 'Family Tree',
+        image: '',
+        desc: `* Display the tree structure including profile pictures of each of the family member
+        * When user press a profile picture, user will be directed to a new screen that shows the details of that user.
+        * When user do a long press on the profile picture, user will be given options to either add parents/ spouse/ child.
+        * User can add existing users as their family member or create dummy users.
+        * User can delete the dummy user and replaced it with an existing user.`,
+        tools: []
+      },
+      {
+        id: 4,
+        title: 'Upload Artefact',
+        image: '',
+        desc: `* Uploading the artefact from the app and stored the data into Firebase (for pictures) and MongoDB (other details).
+        * The uploaded artefact will be displayed on the user's profile screen, timeline and gallery.`,
+        tools: []
+      },
+      {
+        id: 5,
+        title: 'Sending Artefact',
+        image: '',
+        desc: `* User can send their own artefact (displayed in their profile/timeline/gallery screen) to other registered family member.
+        * When user tapped the "Send Artefact" button, their artefact will not be visible in the profile screen and the ownership details will be changed onto the recipient.`,
+        tools: []
+      },
+      {
+        id: 6,
+        title: 'Item Detail Page',
+        image: '',
+        desc: `* When the artefact is selected, the item detail screen will appear
+        * Display details of the artefact including: Name, Value, Description, Date
+        * If Edit button is tapped, the page will become editable
+        * User can fill in fields to edit the details of the artefact`,
+        tools: []
+      },
+      {
+        id: 7,
+        title: 'Timeline',
+        image: '',
+        desc: `* Display artefacts of all users in the family tree with the corresponding time.
+        * Artefacts are sorted based on the dates, starting from the most recent to the most oldest.`,
+        tools: []
+      },
+      {
+        id: 8,
+        title: 'Gallery',
+        image: '',
+        desc: `* Display artefacts in a 3 column grid format of all users in the family tree.
+        * Display artefacts that are only belong to those in the user's family`,
+        tools: []
+      },
+      {
+        id: 9,
+        title: 'Notification',
+        image: '',
+        desc: `* User will receive a notification when someone passed an artefact to them.
+        * The history of the notification will be displayed in the notification screen.`,
+        tools: []
+      },
+      {
+        id: 10,
+        title: '',
+        image: '',
+        desc: ``,
+        tools: ['React Native', 'JavaScript', 'Android', 'Firebase']
+      },
     ],
     tools: ['React Native', 'JavaScript', 'Android', 'Firebase'],
     summary: {
@@ -124,8 +210,43 @@ const projects = [
         id: 0,
         title: '',
         image: gps,
-        desc: 'A web app to let new students/residents of Melbourne to discover unique places and events within the city.',
+        desc: 'A web app to let new students/residents of Melbourne to discover unique places and events within the city. The challenge we are trying to tackle is helping new international students in Melbourne to feel comfortable to their new environment. Mental health issues, such as loneliness, can affect students which may lead to poor health. Our web application was created as a platform for students who are new to Melbourne by allowing them to discover new events and places according to their interests.',
         tools: []
+      },
+      {
+        id: 1,
+        title: 'Home Page',
+        image: '',
+        desc: 'The home page greets users with a map-based search where they can visually locate events around them in the map which may interest them. Being a student means your time is occupied with studies and having a map-based search for events, students can look for events quickly. A short glimpse of the events is accessible by clicking on the point in the map, as glimpse of events around Melbourne.',
+        tools: []
+      },
+      {
+        id: 2,
+        title: 'Sign up & Sign in',
+        image: '',
+        desc: 'To access the rest of the features of the website, they must sign up as a member which allows users to personally curate their interests. Members have a profile page containing their information such as: personal details, interests, friends, the places and events they’ve visited and reviews they’ve written. This will simplify the effort required to find information and allows a more personalised and immediate experience.',
+        tools: []
+      },
+      {
+        id: 3,
+        title: 'Filter Events',
+        image: '',
+        desc: 'Signing up allows users to not only search events and places but also view event details and join events they are interested in. They can also filter out events by tags, making it easier to discover events and places they want to join based on their interests. This can keep international students updated on places and events that both suit their interests and are related to their culture and home country. Therefore, they can find a place that feels like home in a foreign land.',
+        tools: []
+      },
+      {
+        id: 4,
+        title: 'Reviews and Ratings',
+        image: '',
+        desc: 'On each event details page users can see the reviews and ratings others have submitted to help them choose events to join. By joining events they like, users can also see others whose have wrote the reviews and the organisers of the events and are likely to come across people with similar interest. Our system allows member to follow and be followed by other members, which is a great way to meet other users with similar interests and potentially make new friends.',
+        tools: [],
+      },
+      {
+        id: 5,
+        title: '',
+        image: '',
+        desc: '',
+        tools: ['JavaScript', 'Pug', 'CSS', 'Firebase', 'Heroku'],
       },
     ],
     tools: ['JavaScript', 'Pug', 'CSS', 'Firebase', 'Heroku'],
